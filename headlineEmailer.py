fdacd4f7c90b2ddc2246813126280878da294edb
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-05-24 06:27:59 -0700

    composed email module

--- headlineEmailer.py
+++ headlineEmailer.py
@@ -38,5 +38,38 @@ content += cnt
 content += ('<br>-------<br>')
 content +=('<br><br>End Of Message')
 
+# This function is to extract the front page links or titile/components that we want
+# function extract_news is passed the url where we are extracting then we are creating
+# is a nice title that says HN Top stories & then extracting the content
+# We are using BeautifulSoup to make a soup of the content
+# Then we are using soup.find_all to find all the td tags with attributes:
+# (class, title, valigh). Then we are creating rows 
+# Then we notice that there is one final row that has a value of MORE
+# so we are exluding that. Then we are returing the function with cnt
+
+
+
+# Let send the email
+
+print('Comosing Email...')
+
+SERVER = 'smtp.gmail.com' # "Your smtp SERVER"
+PORT = 587 # your port number
+FROM = '' # your from email id **Be Careful pusing this to repo**
+TO = '' # your to email id  **Becareful pushing this to repo**
+PASS = '' # your email id's password **Becareful pushing this to repo**
+
+# fp = open(file_name, 'rb')
+# Create a text/plain message
+# msg = MIMEText('')
+msg = MIMEMultipart()
+
+# msg.add_header('Content-Disposition', 'attachment', filename='empty.txt')
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+msg['From'] = FROM
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+# fp.close()
 
 
